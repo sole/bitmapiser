47199b1d4c7b287f533f82ae33dd6df7dc9b1387
sole <sole@mozilla.com>
2014-05-20 11:55:37 +0100

    Build output filenames according to sizes

--- bitmapiser.js
+++ bitmapiser.js
@@ -28,8 +28,35 @@ function Bitmapiser(options) {
     console.log(child); // XXX
 
     // Then resize down
+    // Load image into bitmap? image?
 
-    
+    // Repeatedly copy and resize for each size
+    sizes.forEach(function(size) {
+      
+      var width, height;
+
+      size = size + '';
+      if(size.indexOf('x') !== -1) {
+        var sizeParts = size.split('x');
+        width = sizeParts[0];
+        height = sizeParts[1];
+      } else {
+        width = size;
+        height = size;
+      }
+
+      // TODO is there a better way to do this? looks a bit... uggghh.
+      width = width * 1;
+      height = height * 1;
+
+      console.log(size, typeof(size), width, height, typeof(width), typeof(height));
+
+      var resizedFileName = printf('%s%dx%d.png', fileName, width, height);
+      var resizedPath = path.join(outputDir, resizedFileName);
+
+      console.log(resizedPath);
+
+    });
 
   }
 
